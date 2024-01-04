bb7aba010c132c19875a817a5b284b2c508e885d
Jordan Karp <jordanmatthew.karp@gmail.com>
2024-01-04 15:27:30 -0800

    Toggle working

--- public/javascripts/darkMode.js
+++ public/javascripts/darkMode.js
@@ -3,23 +3,26 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle')
 
 const enableDarkMode = () => {
     document.body.classList.add('darkMode')
+    darkModeToggle.innerHTML = 'Light Mode'
+
     localStorage.setItem('darkMode', 'enabled')
 }
 const disableDarkMode = () => {
     document.body.classList.remove('darkMode')
+    darkModeToggle.innerHTML = 'Dark Mode'
     localStorage.setItem('darkMode', 'disabled')
 }
 
 if (darkMode == 'enabled') {
     enableDarkMode();
+    darkModeToggle.innerHTML = 'Light Mode'
 }
 
 darkModeToggle.addEventListener('click', () => {
-    console.log('test')
-    // darkMode = localStorage.getItem('darkMode')
-    // if (darkMode !== 'enabled') {
-    //     enableDarkMode();
-    // } else {
-    //     disableDarkMode();
-    // }
+    darkMode = localStorage.getItem('darkMode')
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 })
